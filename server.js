8bf1fe07ba1c176438453b8f18c399d4a56f5b14
len0rd <tylerm15@gmail.com>
2018-07-27 13:32:34 -0400

    added basic contact page, fixed server and nav to handle it

--- server.js
+++ server.js
@@ -6,13 +6,18 @@ console.log('Hello');
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load an ejs view file
-
-app.get('/', function(req, res) {
-    res.render('pages/index');
-});
-
 // add folder for static content:
 app.use(express.static(__dirname + '/assets'));
+
+app.get(/\/.*/, function(req, res) {
+    console.log(req.path);
+    let pathname = 'pages' + req.path;
+
+    if ((pathname)[pathname.length - 1] === '/') {
+        pathname += 'index';
+    }
+
+    res.render(pathname);
+});
 
 app.listen(8090);

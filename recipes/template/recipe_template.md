97fb2a61fe8fca8e3d3624508a971a4802f345cb
len0rd <len0rd@users.noreply.github.com>
2022-07-03 09:53:37 -0400

    very simple post request that writes recipe to markdown working

---
+++ recipes/template/recipe_template.md
@@ -0,0 +1,13 @@
+# ${recipe_name_input}
+
+${tags}
+
+## Ingredients
+
+Measure | Unit | Weight | Ingredient
+--------|------|--------|-----------
+${ingredient_input}
+
+## Instructions
+
+${instruction_input}
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const PORT = 8090;
 var express = require('express');
+// formidable helps get JSON POST request content
 const formidable = require('express-formidable');
+var compile = require('es6-template-strings/compile'),
+    resolveToString = require('es6-template-strings/resolve-to-string'),
+    fs = require('fs');
+// create a function that can convert from recipe template literal -> new recipe markdown
+const recipeMarkdownDir = "./recipes/";
+recipe_template_raw = fs.readFileSync(recipeMarkdownDir + "template/recipe_template.md", "utf-8");
+compiledRecipeTemplate = compile(recipe_template_raw);
+
 var app = express();
 
 app.use(formidable());
@@ -43,9 +52,36 @@ app.get(/\/.*/, function (req, res) {
     res.render(pathname, { "page": page });
 });
 
+function toCamelCase(str) {
+    return str.replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+        return index == 0 ? word.toLowerCase() : word.toUpperCase();
+    }).replace(/\s+/g, '');
+}
+
+// When a user successfully submits a new recipe a post request with
+// JSON content of recipe data ends up here
 app.post("/new_recipe", function (req, res) {
-    console.log(JSON.stringify(req.fields));
-    res.send('mission success');
+    let dataIn = req.fields;
+
+    filename = recipeMarkdownDir + toCamelCase(dataIn.recipe_name_input) + ".md";
+    if (fs.existsSync(recipeMarkdownDir + filename)) {
+        res.status(409).send(`Already have a recipe with name "${dataIn.recipe_name_input}"`);
+        return;
+    }
+
+    // TODO: more input checking
+
+    // add a '#' to all the tags in the input list
+    formatted_tags = dataIn.tag_input.split(",")
+        .map(elem => "#" + elem)
+        .join(" ");
+
+    dataIn.tags = formatted_tags
+
+    formattedMarkdownRecipe = resolveToString(compiledRecipeTemplate, dataIn);
+    fs.writeFileSync(filename, formattedMarkdownRecipe, 'utf-8');
+
+    res.send('Successfully added!');
 });
 
 app.listen(PORT);
